9f3a6fb0ca078353dc61673903ad478627f2962f
Harit Sah <HaritS@hcl.com>
2019-04-25 09:47:51 +0530

    UAA: changes

--- src/main/java/com/hcl/cloud/uaa/constant/UaaConstant.java
+++ src/main/java/com/hcl/cloud/uaa/constant/UaaConstant.java
@@ -2,5 +2,5 @@ package com.hcl.cloud.uaa.constant;
 
 public class UaaConstant {
 
-	public static final long TOKEN_EXP_VALIDITY= 600000;
+	public static final long TOKEN_EXP_VALIDITY= 3600000; //600000;
 }
